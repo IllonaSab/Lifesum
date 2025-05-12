adb2ac4b87ca597674743d4d5e51daa7130be62a
IllonaSab <sabouillona@gmail.com>
2025-05-12 09:47:23 +0200

    page.js

--- front/src/app/signup/step1/page.js
+++ front/src/app/signup/step1/page.js
@@ -1,6 +1,6 @@
 'use client';
-import Step1 from '../../../components/Step1';
+import Step1 from '../../../components/Step1'; //import du composant
 
 export default function Step1Page() {
-  return <Step1 />;
+  return <Step1 />; //Afficher la page
 }
